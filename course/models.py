b9954fb23fd9e2320f58960462a7a92173f24978
Azamjon <azamjonusmon10@gmail.com>
2023-04-19 23:22:06 +0500

    add pricing model

--- course/models.py
+++ course/models.py
@@ -8,3 +8,14 @@ class Course(models.Model):
     month = models.PositiveSmallIntegerField()
     student = models.PositiveSmallIntegerField()
     books = models.PositiveSmallIntegerField()
+
+class Pricing(models.Model):
+    name = models.CharField(max_length=100)
+    month =models.PositiveSmallIntegerField()
+    test =models.CharField(max_length=256)
+    icon = models.CharField(max_length=70)
+    disk = models.PositiveSmallIntegerField(default=50)
+    email = models.PositiveSmallIntegerField(default=50)
+    bandwidth = models.PositiveSmallIntegerField(default=50)
+    subdomain = models.PositiveSmallIntegerField(default=10)
+    domain = models.PositiveSmallIntegerField(default=50)
