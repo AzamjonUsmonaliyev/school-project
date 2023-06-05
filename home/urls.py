c57fbd570dec7fcc8cba28c6b6a25ef129e8d113
Azamjon <azamjonusmon10@gmail.com>
2023-06-05 23:55:19 +0500

    add home page 8/10

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,4 @@ from .views import home,blog
 
 urlpatterns = [
     path('',home,name='home'),
-    path('blog/',blog,name='blog'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from .models import *
+from .models import MainImage,About,Card,History,Student ,Testimonials,Partner
 
 def home(request):
     main_image = MainImage.objects.all()
     about = About.objects.all()
+    card = Card.objects.all()
+    history = History.objects.all()
+    student = Student.objects.all()
+    testimoials = Testimonials.objects.all()
+    partner = Partner.objects.all()
+    data = {
+        'main_image':main_image,
+        'about':about,
+        'cards':card[::2],
+        'cards1':card[1::2],
+        'history':history,
+        'Student':student[0],
+        'testimoials':testimoials,
+        'partner':partner
 
-    return render(request,'index.html')
+    }
+
+    return render(request,'index.html',data)
 def blog(request):
     return render(request,'blog.html')
